010980d442d8e13c54691dd41ed64164a74833a0
AlvaroCori <bacs200018@gmail.com>
2022-04-08 06:04:38 -0400

    initial commit alv

---
+++ app/src/main/java/edu/bo/confesionario/Publications.kt
@@ -0,0 +1,16 @@
+package edu.bo.confesionario
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class Publications : AppCompatActivity() {
+    lateinit var toolbar: ActionBar
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_publications)
+        //toolbar = supportActionBar!!
+        //val bottomNavigation: BottomNavigationView = findViewById(R.id.navigationView)
+    }
+}
