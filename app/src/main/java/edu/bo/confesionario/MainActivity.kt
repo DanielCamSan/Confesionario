bfc8c37c4f7259ac7f7cfc73b1957cd6331c9531
AlvaroCori <bacs200018@gmail.com>
2022-04-11 01:25:00 -0400

    fragments added

--- app/src/main/java/edu/bo/confesionario/MainActivity.kt
+++ app/src/main/java/edu/bo/confesionario/MainActivity.kt
@@ -1,12 +1,18 @@
 package edu.bo.confesionario
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_publications)
+        setContentView(R.layout.activity_main)
+        val intent = Intent(this, Publications::class.java)
+        // To pass any data to next activity
+        //intent.putExtra("keyIdentifier", value)
+        // start your next activity
+        startActivity(intent)
         //app:menu="@menu/publications_menu"
     }
 }
--- app/src/main/java/edu/bo/confesionario/Publications.kt
+++ app/src/main/java/edu/bo/confesionario/Publications.kt
@@ -7,10 +7,49 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class Publications : AppCompatActivity() {
     lateinit var toolbar: ActionBar
+
+    private val navigation_view: BottomNavigationView
+        get() = findViewById(R.id.principal_bottom_navigation_view)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_publications)
+        setContentView(R.layout.activity_publications)
         //toolbar = supportActionBar!!
         //val bottomNavigation: BottomNavigationView = findViewById(R.id.navigationView)
+        navigation_view.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.bottom_navigation_all -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.principal_frame_layout, PublicationAllFragment())
+                        .commit()
+                    true
+                }
+                R.id.bottom_navigation_books -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.principal_frame_layout, PublicationsBooksFragment())
+                        .commit()
+                    true
+                }
+                R.id.bottom_navigation_parties -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.principal_frame_layout, PublicationsPartiesFragment())
+                        .commit()
+                    true
+                }
+                R.id.bottom_navigation_classes -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.principal_frame_layout, PublicationsClassesFragment())
+                        .commit()
+                    true
+                }
+                R.id.bottom_navigation_confesions -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.principal_frame_layout, PublicationsConfesionsFragment())
+                        .commit()
+                    true
+                }
+                else -> true
+            }
+        }
+
     }
 }
