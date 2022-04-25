3f04e1183fd7bfa2dbe63f4981c7c3110c0bbe97
DanielCamSan <daniel.camacho.santacruz.5c@gmail.com>
2022-04-25 13:26:36 -0400

    dani feedback

--- app/src/main/java/edu/bo/confesionario/Confesion.kt
+++ app/src/main/java/edu/bo/confesionario/Confesion.kt
@@ -5,22 +5,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.Switch
 
 
 class Confesion : AppCompatActivity() {
     private val btn_back : Button
         get() = findViewById(R.id.btn_back)
-
-    private val goBackBtn : Button
-        get() =  findViewById(R.id.go_back_btn)
-
-
+    private val btn_publish : Button
+        get() = findViewById(R.id.btn_publish)
     private val toolBarLogoutBtn : ImageButton
         get() =  findViewById(R.id.toolBarLogoutBtn)
 
     private val appInfoBtn : ImageButton
         get() =  findViewById(R.id.infoBtn)
-
+    private val switch_anonymous: Switch
+        get() =  findViewById(R.id.switch_anonymous)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_confesion)
@@ -43,5 +42,15 @@ class Confesion : AppCompatActivity() {
             this.overridePendingTransition(0, 0);
 
         }
+        /*switch_anonymous.setOnCheckedChangeListener{
+            switch_anonymous.thumbTintList=
+        }*/
+        btn_publish.setOnClickListener{
+            val intent = Intent(this, Publications::class.java)
+            startActivity(intent)
+            this.overridePendingTransition(0, 0);
+
+        }
+
     }
 }
