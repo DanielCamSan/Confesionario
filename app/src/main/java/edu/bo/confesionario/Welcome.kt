c18c86def38e7c66e847a1b129e164b42e0f4b2d
AlvaroCori <bacs200018@gmail.com>
2022-04-26 23:47:07 -0400

    last correction

--- app/src/main/java/edu/bo/confesionario/Welcome.kt
+++ app/src/main/java/edu/bo/confesionario/Welcome.kt
@@ -10,9 +10,9 @@ class Welcome : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
-        //Handler().postDelayed({
-        val intent = Intent(this, Publications::class.java)
-        startActivity(intent)
-        //}, 0)
+        Handler().postDelayed({
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }, 0)
     }
 }
