037608e9948f78f0ad598e369e03c3cc56bfc4da
dussanfreire <dussan.freire@ucb.edu.bo>
2022-05-16 12:31:09 -0400

    firebase project created and autentication done

--- app/src/main/java/edu/bo/confesionario/IndividualConfession.kt
+++ app/src/main/java/edu/bo/confesionario/IndividualConfession.kt
@@ -5,9 +5,13 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
 import edu.bo.confesionario.adapter.CommentAdapter
 import java.text.SimpleDateFormat
 import java.util.*
@@ -43,7 +47,13 @@ class IndividualConfession : AppCompatActivity() {
         initRecyclerView()
 
         toolBarLogoutBtn.setOnClickListener{
-            val intent = Intent(this, Login::class.java)
+            FirebaseAuth.getInstance().signOut()
+            GoogleSignIn.getClient(
+                this,
+                GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).build()
+            ).signOut()
+            Toast.makeText(this, R.string.logout_msg, Toast.LENGTH_SHORT).show()
+            val intent = Intent(applicationContext, Login::class.java)
             startActivity(intent)
             this.overridePendingTransition(0, 0);
         }
