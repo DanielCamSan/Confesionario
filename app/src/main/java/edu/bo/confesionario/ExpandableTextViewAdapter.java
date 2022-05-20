9fc5fb0c66e3df4274c7437228af85efbf49b374
Juslan <juslanvargas@gmail.com>
2022-05-20 07:53:14 -0400

    One List item per time

--- app/src/main/java/edu/bo/confesionario/ExpandableTextViewAdapter.java
+++ app/src/main/java/edu/bo/confesionario/ExpandableTextViewAdapter.java
@@ -14,53 +14,54 @@ import org.w3c.dom.Text;
 
 public class ExpandableTextViewAdapter extends BaseExpandableListAdapter {
     Context context;
-    String[]sections={
+    String[] sections = {
             "SERVICIO AL CLIENTE",
             "POLITICA DE PRIVACIDAD",
             "ACERCA DE LA APLICACION",
             "CONTACTANOS"
     };
-    String[][]description={
+    String[][] description = {
             {
-                "Horario del centro de llamadas:\n" +
-                        "Lun-vie 9 am-7pm EST\n" +
-                        "El mejor momento para marcar:\n" +
-                        "10:45 am\n" +
-                        "Navega por el laberinto del teléfono a un humano:\n" +
-                        "Presione 2 y luego 1\n" +
-                        "Espera promedio\n" +
-                        "Espera actual\n" +
-                        "7 7\n" +
-                        "Rango (entre números de teléfono):\n" +
-                        "1\n" +
-                        "Rango (general):\n" +
-                        "1\n" +
-                        "Métodos alternativos:\n" +
-                        "teléfono, web\n" +
-                        "Es recomendable enviar un correo a los mensajes de contactanos."
+                    "Horario del centro de llamadas:\n" +
+                            "Lun-vie 9 am-7pm EST\n" +
+                            "El mejor momento para marcar:\n" +
+                            "10:45 am\n" +
+                            "Navega por el laberinto del teléfono a un humano:\n" +
+                            "Presione 2 y luego 1\n" +
+                            "Espera promedio\n" +
+                            "Espera actual\n" +
+                            "7 7\n" +
+                            "Rango (entre números de teléfono):\n" +
+                            "1\n" +
+                            "Rango (general):\n" +
+                            "1\n" +
+                            "Métodos alternativos:\n" +
+                            "teléfono, web\n" +
+                            "Es recomendable enviar un correo a los mensajes de contactanos."
             },
             {
-                "Esta Política de Privacidad se aplica a todos nuestros juegos, ya se jueguen en nuestro sitio web www.king.com.\n" +
-                        "\n" +
-                        "En dispositivos móviles, PC o en otras plataformas como Facebook. También se aplica a nuestras actividades de marketing y publicidad.\n" +
-                        "\n" +
-                        "En todas las plataformas y otros servicios que podemos proporcionarle de vez en cuando. En esta Política de Privacidad, nos referimos a nuestros"
+                    "Esta Política de Privacidad se aplica a todos nuestros juegos, ya se jueguen en nuestro sitio web www.king.com.\n" +
+                            "\n" +
+                            "En dispositivos móviles, PC o en otras plataformas como Facebook. También se aplica a nuestras actividades de marketing y publicidad.\n" +
+                            "\n" +
+                            "En todas las plataformas y otros servicios que podemos proporcionarle de vez en cuando. En esta Política de Privacidad, nos referimos a nuestros"
             },
             {
-                "Pulse el botón del Mapa del Reino situado en la esquina inferior izquierda para ir al Mapa del Reino, y luego pulse el botón del globo terráqueo para ir al Mapa del Mundo.\n" + "Pulsa sobre un reino para ver su mapa. Toca una casilla vacía y selecciona Reubicar o Migrar para transferir tu territorio. Los jugadores sólo pueden trasladarse a un reino en el que la migración esté restringida si es más antiguo que su reino de origen.\n"
+                    "Pulse el botón del Mapa del Reino situado en la esquina inferior izquierda para ir al Mapa del Reino, y luego pulse el botón del globo terráqueo para ir al Mapa del Mundo.\n" +
+                            "Pulsa sobre un reino para ver su mapa. Toca una casilla vacía y selecciona Reubicar o Migrar para transferir tu territorio. Los jugadores sólo pueden trasladarse a un reino en el que la migración esté restringida si es más antiguo que su reino de origen.\n"
             },
             {
-                "Contactenos en:\n" +
-                        "\n" +
-                        "abc2234@gmail.com\n" +
-                        "androidlover@gmail.com\n" +
-                        "\n" +
-                        "Visite nuestra pagina en:\n" +
-                        "\n" +
-                        "\n" +
-                        "https://www.facebook.com/1182906556/videos/10220903002183147/\n" +
-                        "\n" +
-                        "Y nuestro canal de youtube:\u2028\u2028https://www.youtube.com/watch?v=riT4nl0T8_M"
+                    "Contactenos en:\n" +
+                            "\n" +
+                            "abc2234@gmail.com\n" +
+                            "androidlover@gmail.com\n" +
+                            "\n" +
+                            "Visite nuestra pagina en:\n" +
+                            "\n" +
+                            "\n" +
+                            "https://www.facebook.com/1182906556/videos/10220903002183147/\n" +
+                            "\n" +
+                            "Y nuestro canal de youtube:\u2028\u2028https://www.youtube.com/watch?v=riT4nl0T8_M"
             }
     };
 
@@ -106,12 +107,12 @@ public class ExpandableTextViewAdapter extends BaseExpandableListAdapter {
     @Override
     public View getGroupView(int i, boolean b, View view, ViewGroup viewGroup) {
 
-        String sectionFaq = (String)getGroup(i);
-        if (view==null){
-            LayoutInflater inflater = (LayoutInflater)this.context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
-            view = inflater.inflate(R.layout.ayuda_collapse,null);
+        String sectionFaq = (String) getGroup(i);
+        if (view == null) {
+            LayoutInflater inflater = (LayoutInflater) this.context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.ayuda_collapse, null);
         }
-        TextView sectionFaq2=view.findViewById(R.id.ayudaTitleView);
+        TextView sectionFaq2 = view.findViewById(R.id.ayudaTitleView);
         sectionFaq2.setTypeface(null, Typeface.BOLD);
         sectionFaq2.setText(sectionFaq);
         return view;
@@ -119,12 +120,12 @@ public class ExpandableTextViewAdapter extends BaseExpandableListAdapter {
 
     @Override
     public View getChildView(int i, int i1, boolean b, View view, ViewGroup viewGroup) {
-        final String descriptionFaq = (String)getChild(i,i1);
-        if (view==null){
-            LayoutInflater inflater = (LayoutInflater)this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            view=inflater.inflate(R.layout.ayuda_expand, null);
+        final String descriptionFaq = (String) getChild(i, i1);
+        if (view == null) {
+            LayoutInflater inflater = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.ayuda_expand, null);
         }
-        TextView descriptionFaq2=view.findViewById(R.id.descriptionAyudaView);
+        TextView descriptionFaq2 = view.findViewById(R.id.descriptionAyudaView);
         descriptionFaq2.setText(descriptionFaq);
         return view;
     }
--- app/src/main/java/edu/bo/confesionario/Help.kt
+++ app/src/main/java/edu/bo/confesionario/Help.kt
@@ -1,45 +1,68 @@
 package edu.bo.confesionario
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ExpandableListView
-import android.widget.ImageButton
-import android.widget.Toast
+import android.widget.*
+import android.widget.ExpandableListView.OnGroupExpandListener
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 
 class Help : AppCompatActivity() {
-    private var expandableTextView: ExpandableListView? = null
-    private var adapter = ExpandableTextViewAdapter(this)
+    private var expandableListView: ExpandableListView? = null
+    private var expandableListAdapter: ExpandableListAdapter? = null
+    private var titleList: List<String>? = null
 
-    private val btnBack : Button
+    private val btnBack: Button
         get() = findViewById(R.id.go_back_btn)
 
-    private val toolBarLogoutBtn : ImageButton
-        get() =  findViewById(R.id.toolBarLogoutBtn)
+    private val toolBarLogoutBtn: ImageButton
+        get() = findViewById(R.id.toolBarLogoutBtn)
 
-    private val appInfoBtn : ImageButton
-        get() =  findViewById(R.id.infoBtn)
+    private val appInfoBtn: ImageButton
+        get() = findViewById(R.id.infoBtn)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_help)
-        expandableTextView=findViewById(R.id.eTV)
-        expandableTextView!!.setAdapter(adapter)
+        expandableListView = findViewById(R.id.eTV)
+        if (expandableListView != null) {
+            val listData = data
+            titleList = ArrayList(listData.keys)
+            expandableListAdapter =
+                CustomExpandableListAdapter(this, titleList as ArrayList<String>, listData)
+            expandableListView!!.setAdapter(expandableListAdapter)
+
+            expandableListView!!.setOnGroupExpandListener(object : OnGroupExpandListener {
+                var lastExpandedPosition = -1
+                override fun onGroupExpand(i: Int) {
+                    if (lastExpandedPosition != -1 && i != lastExpandedPosition) {
+                        expandableListView!!.collapseGroup(lastExpandedPosition)
+                    }
+                    lastExpandedPosition = i
+                }
+            })
+
+/*            expandableListView!!.setOnGroupCollapseListener { groupPosition ->
+                Toast.makeText(
+                    applicationContext,
+                    (titleList as ArrayList<String>)[groupPosition] + " List Collapsed.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }*/
+        }
         btnBack.findViewById<Button>(R.id.go_back_btn)
-        btnBack.setOnClickListener{
+        btnBack.setOnClickListener {
             finish()
         }
-        appInfoBtn.setOnClickListener{
+        appInfoBtn.setOnClickListener {
             val intent = Intent(this, Help::class.java)
             startActivity(intent)
             this.overridePendingTransition(0, 0);
 
         }
-        toolBarLogoutBtn.setOnClickListener{
+        toolBarLogoutBtn.setOnClickListener {
             FirebaseAuth.getInstance().signOut()
             GoogleSignIn.getClient(
                 this,
@@ -49,8 +72,79 @@ class Help : AppCompatActivity() {
             val intent = Intent(applicationContext, Login::class.java)
             startActivity(intent)
             this.overridePendingTransition(0, 0);
-
         }
-
     }
+
+    val data: HashMap<String, List<String>>
+        get() {
+            val listData = HashMap<String, List<String>>()
+
+            val serviceDescription = ArrayList<String>()
+            serviceDescription.add(
+                """
+                    Horario del centro de llamadas:
+                    Lun-vie 9 am-7pm EST
+                    El mejor momento para marcar:
+                    10:45 am
+                    Navega por el laberinto del teléfono a un humano:
+                    Presione 2 y luego 1
+                    Espera promedio
+                    Espera actual
+                    7 7
+                    Rango (entre números de teléfono):
+                    1
+                    Rango (general):
+                    1
+                    Métodos alternativos:
+                    teléfono, web
+                    Es recomendable enviar un correo a los mensajes de contactanos.
+                    """.trimIndent()
+            )
+
+            val policeDescription = ArrayList<String>()
+            policeDescription.add(
+                """
+                    Esta Política de Privacidad se aplica a todos nuestros juegos, ya se jueguen en nuestro sitio web www.king.com.
+                    
+                    En dispositivos móviles, PC o en otras plataformas como Facebook. También se aplica a nuestras actividades de marketing y publicidad.
+                    
+                    En todas las plataformas y otros servicios que podemos proporcionarle de vez en cuando. En esta Política de Privacidad, nos referimos a nuestros
+                    """.trimIndent()
+            )
+
+
+            val aboutDescription = ArrayList<String>()
+            aboutDescription.add(
+                """
+                    Pulse el botón del Mapa del Reino situado en la esquina inferior izquierda para ir al Mapa del Reino, y luego pulse el botón del globo terráqueo para ir al Mapa del Mundo.
+                    Pulsa sobre un reino para ver su mapa. Toca una casilla vacía y selecciona Reubicar o Migrar para transferir tu territorio. Los jugadores sólo pueden trasladarse a un reino en el que la migración esté restringida si es más antiguo que su reino de origen.
+                    
+                    """.trimIndent()
+            )
+
+
+            val contactDescription = ArrayList<String>()
+            contactDescription.add(
+                """
+                    Contactenos en:
+                    
+                    abc2234@gmail.com
+                    androidlover@gmail.com
+                    
+                    Visite nuestra pagina en:
+                    
+                    
+                    https://www.facebook.com/1182906556/videos/10220903002183147/
+                    
+                    Y nuestro canal de youtube:  https://www.youtube.com/watch?v=riT4nl0T8_M
+                    """.trimIndent()
+            )
+
+            listData["SERVICIO AL CLIENTE"] = serviceDescription
+            listData["POLITICA DE PRIVACIDAD"] = policeDescription
+            listData["ACERCA DE LA APLICACION"] = aboutDescription
+            listData["CONTACTANOS"] = contactDescription
+
+            return listData
+        }
 }
